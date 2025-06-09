4d985ad8d6f2ffad51e514e59d527f527db10d18
Brian Piert <SucellosMusic@gmail.com>
2025-06-08 22:28:45 -0400

    initial commit and setup

---
+++ init.lua
@@ -0,0 +1,21 @@
+vim.api.nvim_set_keymap("i", "jk", "<ESC>", {noremap = true, silent = true})
+vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+require("config.lazy")
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local configs = require("nvim-treesitter.configs")
+configs.setup({
+	ensure_installed = { "c", "lua", "cpp"},
+	sync_install  = false,
+	highlight = {enable = true},
+	indent = {enable = true}
+	})
+
+require("config.cmakeconfig")
+
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {silent = true})
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,4 @@
+return
+{
+	{'iagorrr/noctishc.nvim' }
+}
